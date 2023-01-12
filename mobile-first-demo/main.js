a968a8694fd2ac98bdb166c25839b3adfa4319e8
Arexan-K <94745953+ArexanK@users.noreply.github.com>
2023-01-12 17:06:06 +0100

    add text

--- mobile-first-demo/main.js
+++ mobile-first-demo/main.js
@@ -3,3 +3,15 @@ const navbarMenu = document.querySelector(".navbar-menu");
 navbarIcon.addEventListener("click", function () {
     navbarMenu.classList.toggle("show");
 });
+
+
+$(document).ready(function () {
+    $('.slider').slick({
+        dots: true,
+        arrows: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});
